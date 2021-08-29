ba89cca6dd9922cf8853bae6315737db7ff8add8
Kingsley Onwupeluonye <kingslex1995@gmail.com>
2021-08-29 13:48:23 +0100

    Creating Settings Screen and Accessing SharedPreferences

--- app/src/main/java/com/gads/notekeeper/MainActivity.java
+++ app/src/main/java/com/gads/notekeeper/MainActivity.java
@@ -1,10 +1,12 @@
 package com.gads.notekeeper;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
 
 import com.google.android.material.navigation.NavigationView;
@@ -17,6 +19,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -47,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.toolbar.setTitle(getString(R.string.app_name));
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,9 +58,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        toolbar.setTitle("NoteKeeper");
-//        setSupportActionBar(toolbar);
+        //Setting the default value for elements in the settings activity
+        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false);
 
         DrawerLayout drawer = binding.drawerLayout;
         mNavigationView = binding.navView;
@@ -88,7 +91,10 @@ public class MainActivity extends AppCompatActivity {
                 else if (id == R.id.nav_courses){
                     displayCourses();
                 }
-
+                else if (id == R.id.nav_share){
+                    //code to interact with the value for favorite social network
+                    handleShare();
+                }
 
                 DrawerLayout drawer = binding.drawerLayout;
                 drawer.closeDrawer(GravityCompat.START);
@@ -97,10 +103,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void handleSelection(String message) {
+    private void handleShare() {
         View view = findViewById(R.id.list_items);
-        Snackbar.make(view,message,Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(view,"Share to - " +
+                PreferenceManager.getDefaultSharedPreferences(this).getString("user_favorite_social", ""),
+                Snackbar.LENGTH_SHORT).show();
     }
+
 
     @Override
     public void onBackPressed() {
@@ -131,14 +140,19 @@ public class MainActivity extends AppCompatActivity {
 
         //Setting up the RecyclerView and LayoutManager
         mRecyclerItems = (RecyclerView) findViewById(R.id.list_items);
+        //Layout Manager for Notes
         mNotesLayoutManager = new LinearLayoutManager(this);
         mRecyclerItems.setLayoutManager(mNotesLayoutManager);
-        mCoursesLayoutManager = new GridLayoutManager(this, 2);
+        //Layout Manager for Courses
+        mCoursesLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.course_grid_span));
 
+        //Reference to the list of notes in the DataManager
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        //Populating the Notes RecyclerAdapter with the list of notes
         mNoteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
-
+        //Reference to the list of courses in the DataManager
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        //Populating the Course RecyclerAdapter with the list of courses
         mCourseRecyclerAdapter = new CourseRecyclerAdapter(this, courses);
 
         displayNotes();
@@ -158,5 +172,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNavHeader();
+    }
+
+    private void updateNavHeader() {
+        NavigationView navigationView = binding.navView;
+        //Reference to NavigationView header
+        View headerView = navigationView.getHeaderView(0);
+        //Reference to the TextViews in the Navigation header
+        TextView textUserName = headerView.findViewById(R.id.text_user_name);
+        TextView textEmailAddress = headerView.findViewById(R.id.text_email_address);
+
+        //Interacting with the Preference System in SettingsActivity
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("user_display_name", "");
+        String emailAddress = pref.getString("user_email_address", "");
+
+        textUserName.setText(userName);
+        textEmailAddress.setText(emailAddress);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/gads/notekeeper/SettingsActivity.java
@@ -0,0 +1,49 @@
+package com.gads.notekeeper;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceGroup;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
